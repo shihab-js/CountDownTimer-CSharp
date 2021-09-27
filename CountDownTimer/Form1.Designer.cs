63af444aee4971bdf9f915b758922aa9227c2114
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-27 21:57:22 +0600

    Project has been done...

--- CountDownTimer/Form1.Designer.cs
+++ CountDownTimer/Form1.Designer.cs
@@ -48,9 +48,10 @@ namespace CountDownTimer
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(242, 97);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.Size = new System.Drawing.Size(0, 65);
             this.label1.TabIndex = 1;
             // 
             // timer1
